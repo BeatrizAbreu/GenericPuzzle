ba0081bb2ea949d4ce10f39a4133ea0db0ebf2bb
Alberto Simões <ambs@cpan.org>
2020-03-15 13:57:46 +0000

    Use a Singleton for RNG

---
+++ Game1/Game1/Scripts/RNG.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace Game1
+{
+
+    public class RNG {
+        static RNG rngen;
+        Random rng;
+        public RNG() {
+            Random tmp = new Random();
+            Int32 seed = tmp.Next();
+            Console.WriteLine($"RANDOM SEED: {seed}");
+            rng = new Random(seed);
+            rngen = this;
+        }
+        public RNG(Int32 seed) {
+            rng = new Random(seed);
+            rngen = this;
+        }
+
+        public static int Next(int limit) { return RNG.rngen.rng.Next(limit); }
+    }
+}
